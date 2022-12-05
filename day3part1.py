7b02224af1a27213c1e696654f61b2183bca4680
xanderoot <45837051+xanderoot@users.noreply.github.com>
2022-12-05 14:52:48 -0800

    day 3

---
+++ day3part1.py
@@ -0,0 +1,67 @@
+'''
+The list of items for each rucksack is given as characters all on a single line. A given rucksack always has the same number of items in each of its two compartments, so the first half of the characters represent items in the first compartment, while the second half of the characters represent items in the second compartment.
+
+For example, suppose you have the following list of contents from six rucksacks:
+
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+
+    The first rucksack contains the items vJrwpWtwJgWrhcsFMMfFFhFp, which means its first compartment contains the items vJrwpWtwJgWr, while the second compartment contains the items hcsFMMfFFhFp. The only item type that appears in both compartments is lowercase p.
+    The second rucksack's compartments contain jqHRNqRjqzjGDLGL and rsFMfFZSrLrFZsSL. The only item type that appears in both compartments is uppercase L.
+    The third rucksack's compartments contain PmmdzqPrV and vPwwTWBwg; the only common item type is uppercase P.
+    The fourth rucksack's compartments only share item type v.
+    The fifth rucksack's compartments only share item type t.
+    The sixth rucksack's compartments only share item type s.
+
+To help prioritize item rearrangement, every item type can be converted to a priority:
+
+    Lowercase item types a through z have priorities 1 through 26.
+    Uppercase item types A through Z have priorities 27 through 52.
+
+In the above example, the priority of the item type that appears in both compartments of each rucksack is 16 (p), 38 (L), 42 (P), 22 (v), 20 (t), and 19 (s); the sum of these is 157.
+
+Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types?
+
+To begin, get your puzzle input.
+
+'''
+#priorities dict generated using openAI
+priorities = {
+ 'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9,
+ 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17,
+ 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25,
+ 'z': 26, 'A': 27, 'B': 28, 'C': 29, 'D': 30, 'E': 31, 'F': 32, 'G': 33,
+ 'H': 34, 'I': 35, 'J': 36, 'K': 37, 'L': 38, 'M': 39, 'N': 40, 'O': 41,
+ 'P': 42, 'Q': 43, 'R': 44, 'S': 45, 'T': 46, 'U': 47, 'V': 48, 'W': 49,
+ 'X': 50, 'Y': 51, 'Z': 52
+ }
+
+
+#open the file
+
+with open('day3input.txt','r') as f: #open file and declare list
+    truck = f.readlines()
+
+truck = [each[:-1] for each in truck] #remove \n from each string
+
+prioritySum = 0
+
+for x in range(len(truck)): # creates a 2d list containing the compartments of each bag 
+    bag = truck[x]
+    compartment1 = set(bag[:len(bag) // 2])
+    compartment2 = set(bag[len(bag) // 2:])
+    matchingItems = compartment1 & compartment2
+    temp = str(matchingItems)[2]
+    prioritySum += priorities[temp]
+print(prioritySum)
+
+
+
+
+#count each string and divide its contents to two dimensional lists.
+
+
